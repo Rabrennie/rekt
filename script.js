8c1cfd606a0740e2b034e15cef0ffb343615f7b2
Rab Rennie <RENNIERAB@gmail.com>
2015-11-16 17:26:18 -0800

    Added text to speech

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var rektText = [],
   currentText,
   el = $('#rektText');
 
+  meSpeak.loadConfig("mespeak_config.json");
+  meSpeak.loadVoice('voices/en/en-us.json');
+
 $.ajax({
   url: "https://rawgit.com/seiyria/status-list/master/rekt-list.md",
   context: document.body
@@ -11,6 +14,7 @@ $.ajax({
   rektText = _.slice(rektText, 1, rektText.length - 2);
   init();
   new Clipboard('.btn');
+  el.click(speak);
   $('#newRekt').click(reRekt);
 });
 
@@ -20,6 +24,7 @@ init = function() {
     if (rektText[pos]) {
       currentText = rektText[pos];
       el.html(currentText);
+      speak()
     } else {
       reRekt();
     }
@@ -32,4 +37,16 @@ reRekt = function() {
   currentText = _.sample(rektText);
   window.location.hash = _.indexOf(rektText, currentText);
   el.html(currentText);
+  speak();
 };
+
+speak = function() {
+  var text = currentText;
+
+  text = _.trim(text, '[x]');
+
+  text = text.split('REKT').join(' REKT ');
+  text = text.split('-').join(' ');
+
+  meSpeak.speak(text);
+}
